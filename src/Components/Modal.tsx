ab358505dd537aa951dd84da8ecd3e17d248ba27
Naral <77269201+NaralC@users.noreply.github.com>
2022-12-16 00:31:07 +0000

    add hero modal

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { TbError404Off } from "react-icons/tb";
 
-type ModalProps = {
-  header: string;
-  body: string;
-};
-
-const Modal: React.FC<ModalProps> = ({ body, header }) => {
+const Modal: React.FC = () => {
   return (
     <div>
-      <div className="modal modal-open modal-middle">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">{header}</h3>
-          <p className="py-4">{body}</p>
+      <div className="modal modal-open modal-middle backdrop-blur-sm">
+        <div className="modal-box text-center items-center flex flex-col">
+          <TbError404Off className="text-5xl my-3"/>
+          <h3 className="font-bold text-xl">
+            Couldn't find the most optimal path
+          </h3>
+          <p className="py-4 text-lg">
+            All paths leading to the finish are blocked!
+          </p>
         </div>
       </div>
     </div>
